409e052782fbfbcbcf313b51d0fc99fb4e79de2a
fery <67243217+ferytell@users.noreply.github.com>
2023-08-07 17:12:46 +0700

    first-commit

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+async function Page() {
+    return (
+        <main>
+            <h1 className="head-text">Onboardonggggg</h1>
+            <UserButton/>
+        </main>
+    )
+}
+
+export default Page;
