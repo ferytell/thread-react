ec149baafb5f152c5393fa7a076cfacb1abb1d24
fery <67243217+ferytell@users.noreply.github.com>
2023-08-08 13:05:47 +0700

    basic header, sidebar and footer

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 async function Page() {
     return (
         <main>
             <h1 className="head-text">Onboardonggggg</h1>
             <UserButton/>
+            <SignOutButton>
+                        <div className="flex cursor-pointer">
+                            <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+                        </div>
+            </SignOutButton>
         </main>
     )
 }
