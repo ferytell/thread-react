97bd68aef950b8aad6dab3e0133dab8f4b6a9e21
fery <67243217+ferytell@users.noreply.github.com>
2023-08-11 16:55:16 +0700

    add database

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,9 @@ import * as z from "zod"
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthings';
+import { usePathname, useRouter } from 'next/navigation';
+import { updateUser } from '@/lib/action/user.action';
+
 
 interface Props {
     user: {
@@ -35,7 +38,8 @@ interface Props {
 const AccountProfile = ( { user, btnTitle }: Props ) => {
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
-
+    const router = useRouter();
+    const pathname =  usePathname();
 
     const form = useForm({ 
         resolver: zodResolver(UserValidation),
@@ -83,6 +87,14 @@ const AccountProfile = ( { user, btnTitle }: Props ) => {
         }
 
         // TODO: update user photo profile
+        await updateUser({
+            userId: user.id, 
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path:pathname  
+        });
     }
 
     return (
