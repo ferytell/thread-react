27efa8ada1ff9509ea18e32795ba6cb832480a09
fery <67243217+ferytell@users.noreply.github.com>
2023-08-11 09:03:14 +0700

    function to update profile pic

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,7 +16,9 @@ import {
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import * as z from "zod"
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthings';
 
 interface Props {
     user: {
@@ -31,6 +33,10 @@ interface Props {
 }
 
 const AccountProfile = ( { user, btnTitle }: Props ) => {
+    const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media");
+
+
     const form = useForm({ 
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -42,14 +48,42 @@ const AccountProfile = ( { user, btnTitle }: Props ) => {
     });
 
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "" ;
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
     }
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-      }
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files);
+
+            if (imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        }
+
+        // TODO: update user photo profile
+    }
 
     return (
         <Form {...form}>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {                                         // on dev server database is allowed to acces nextjs
+      serverActions: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [                                     // list of addres that allowed to acces nextjs
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+        },
+        {
+          protocol: "https",
+          hostname: "images.clerk.dev",
+        },
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+      ],
+      typescript: {
+        ignoreBuildErrors: true,
+      },
+    },
+  };
+  
+  module.exports = nextConfig;
