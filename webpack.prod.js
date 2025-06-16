db12d317a51b2416e47d4b52e84f64d5506f93c2
ferytell <darkstar2709@gmail.com>
2025-06-16 21:59:49 +0700

    dsdfds

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,14 +2,9 @@ const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    publicPath: process.env.publicPath || '/',
-  },
   module: {
     rules: [
       {
@@ -30,41 +25,5 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-    // new CopyPlugin({
-    //   patterns: [
-    //     { from: 'src/manifest.json', to: 'manifest.json' },
-    //     { from: 'src/images', to: 'images' },
-    //     { from: 'src/favicon.png', to: 'favicon.png' },
-    //   ],
-    // }),
-    new GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: [
-        {
-          urlPattern: /\.(?:png|jpg|jpeg|svg|json)$/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'images-cache',
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            },
-          },
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.googleapis\.com/,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'google-fonts-stylesheets',
-          },
-        },
-      ],
-    }),
-  ],
+  plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
 });
