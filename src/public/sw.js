7694bea9ecf163ac5af19fb5c0acddc6f7f00730
ferytell <darkstar2709@gmail.com>
2025-06-16 22:45:50 +0700

    add ofline html

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,20 +1,25 @@
 const CACHE_NAME = 'sharestory-v1';
+
+const BASE_URL = self.location.pathname.replace(/\/sw\.js$/, '/');
+
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/styles/styles.css',
-  '/styles/responsives.css',
-  '/images/sharestory.png',
+  `${BASE_URL}`,
+  `${BASE_URL}index.html`,
+  `${BASE_URL}offline.html`,
+  `${BASE_URL}styles/styles.css`,
+  `${BASE_URL}styles/responsives.css`,
+  `${BASE_URL}images/sharestory.png`,
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
 ];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).catch(() => caches.match('/offline.html')));
+    event.respondWith(fetch(event.request).catch(() => caches.match('offline.html')));
   } else {
     event.respondWith(
       caches.match(event.request).then((response) => response || fetch(event.request)),
