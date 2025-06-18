102b13d791c95e4d726d313f9aa0156a98b87152
ferytell <darkstar2709@gmail.com>
2025-06-18 21:37:44 +0700

    sdfsdf

--- src/scripts/pages/404/404-page.js
+++ src/scripts/pages/404/404-page.js
@@ -4,7 +4,7 @@ export default class NotFoundPage {
       <section class="not-found">
         <div class="container">
           <div class="not-found__content">
-            <img src="/images/404.png" alt="404 Not Found" class="not-found__image" />
+            <img src="images/404.png" alt="404 Not Found" class="not-found__image" />
 
             <h1>404 - Page Not Found</h1>
             <p>Sowwry, the page you're looking for doesn't exist or has been moved.</p>
