7c527bc066c7db178fd1cef465bf6b358ff51dbd
ferytell <darkstar2709@gmail.com>
2025-06-18 21:46:38 +0700

    asd

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -31,6 +32,40 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin(),
     new CopyWebpackPlugin({
       patterns: [{ from: 'src/manifest.json', to: 'manifest.json' }],
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [
+        {
+          // Asset
+          urlPattern: /\.(?:js|css|html|png|jpg|jpeg|svg|woff2?)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'assets-cache',
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 hari
+            },
+          },
+        },
+        {
+          // API call to /stories
+          urlPattern: ({ url }) => url.pathname.includes('/stories'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'stories-api-cache',
+            networkTimeoutSeconds: 3,
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 7 * 24 * 60 * 60, // 7 hari
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
     }),
   ],
 });
