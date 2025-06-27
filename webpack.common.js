c9776016a97b67bb1e9495e224cd1da7b6862b03
ferytell <darkstar2709@gmail.com>
2025-06-27 13:48:21 +0700

    offline

--- webpack.common.js
+++ webpack.common.js
@@ -37,10 +37,6 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/')
-        },
-        {
-          from: path.resolve(__dirname, 'src/public/offline.html'),
-          to: path.resolve(__dirname, 'dist/offline.html')
         }
       ]
     })
